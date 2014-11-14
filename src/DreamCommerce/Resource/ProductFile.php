e3fe3eebbb0722ebf6ee83d1b5add34782978203
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2014-11-14 13:10:25 +0100

    Auction, AuctionHouse, ProductFile

---
+++ src/DreamCommerce/Resource/ProductFile.php
@@ -0,0 +1,13 @@
+<?PHP
+namespace DreamCommerce\Resource;
+
+use DreamCommerce\Resource;
+use DreamCommerce\Client;
+
+class ProductFile extends Resource{
+    
+    public function __construct(Client $client){
+        return parent::__construct($client, 'product-files');
+    }
+    
+}
