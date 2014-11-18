7611f94638b24dd148d538a29d00f2952a91563b
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2014-11-18 16:27:19 +0100

    fixed autoloader

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
-spl_autoload_register();
+spl_autoload_register(function($class){
+    $class = str_replace('\\', '/', $class);
+    require 'src/'.$class.'.php';
+});
