0995a6ceb10d371d7f659fb76185d7a8d5989ede
Przemysław Pawliczuk <przemyslaw.pawliczuk@shoper.pl>
2014-11-18 16:54:52 +0100

    mysql dump updated

--- examples/sql/mysql.sql
+++ examples/sql/mysql.sql
@@ -16,39 +16,47 @@ CREATE TABLE IF NOT EXISTS `access_tokens` (
   `shop_id` int(10) unsigned DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `access_token` char(50) COLLATE utf8_polish_ci DEFAULT NULL,
-  `refresh_token` char(50) COLLATE utf8_polish_ci DEFAULT NULL,
+  `access_token` char(50) DEFAULT NULL,
+  `refresh_token` char(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `shop_id` (`shop_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  KEY `shop_id` (`shop_id`),
+  CONSTRAINT `FK_access_tokens_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table app.access_tokens: ~0 rows (approximately)
+/*!40000 ALTER TABLE `access_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `access_tokens` ENABLE KEYS */;
 
 
 -- Dumping structure for table app.billings
 CREATE TABLE IF NOT EXISTS `billings` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `shop_id` int(10) unsigned DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `shop_id` (`shop_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  KEY `shop_id` (`shop_id`),
+  CONSTRAINT `FK_billings_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table app.billings: ~0 rows (approximately)
+/*!40000 ALTER TABLE `billings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billings` ENABLE KEYS */;
 
 
 -- Dumping structure for table app.shops
 CREATE TABLE IF NOT EXISTS `shops` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `shop` char(50) COLLATE utf8_polish_ci DEFAULT NULL,
-  `shop_url` varchar(512) COLLATE utf8_polish_ci DEFAULT NULL,
-  `auth_code` char(50) COLLATE utf8_polish_ci DEFAULT NULL,
+  `shop` char(40) DEFAULT NULL,
+  `shop_url` varchar(512) DEFAULT NULL,
+  `auth_code` char(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `shop` (`shop`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table app.shops: ~0 rows (approximately)
+/*!40000 ALTER TABLE `shops` DISABLE KEYS */;
+/*!40000 ALTER TABLE `shops` ENABLE KEYS */;
 
 
 -- Dumping structure for table app.subscriptions
@@ -57,10 +65,13 @@ CREATE TABLE IF NOT EXISTS `subscriptions` (
   `shop_id` int(10) unsigned NOT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `shop_id` (`shop_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  KEY `shop_id` (`shop_id`),
+  CONSTRAINT `FK_subscriptions_shops` FOREIGN KEY (`shop_id`) REFERENCES `shops` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table app.subscriptions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `subscriptions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subscriptions` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
