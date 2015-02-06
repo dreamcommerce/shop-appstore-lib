78c84c9770fd57bd026c15a2c82b64265707ab2c
Adrian Słowik <kontakt@adrianslowik.pl>
2015-02-06 12:41:08 +0100

    metafields support

--- src/DreamCommerce/Resource/Metafield.php
+++ src/DreamCommerce/Resource/Metafield.php
@@ -5,7 +5,12 @@ use DreamCommerce\Resource;
 use DreamCommerce\Client;
 
 class Metafield extends Resource{
-    
+
+    const TYPE_INT = 1;
+    const TYPE_FLOAT = 2;
+    const TYPE_STRING = 3;
+    const TYPE_BLOB = 4;
+
     public function __construct(Client $client){
         return parent::__construct($client, 'metafields');
     }
