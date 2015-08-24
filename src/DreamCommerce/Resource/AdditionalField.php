408862fd2fa5c6771097617a2fd8f14ac57b0925
Michał Korus <michal.korus@shoper.pl>
2015-08-24 17:38:07 +0200

    add useful constants

--- src/DreamCommerce/Resource/AdditionalField.php
+++ src/DreamCommerce/Resource/AdditionalField.php
@@ -12,5 +12,22 @@ use DreamCommerce\Resource;
  */
 class AdditionalField extends Resource
 {
+    const FIELD_TYPE_TEXT = 1;
+    const FIELD_TYPE_CHECKBOX = 2;
+    const FIELD_TYPE_SELECT = 3;
+    const FIELD_TYPE_FILE = 4;
+    const FIELD_TYPE_HIDDEN = 5;
+
+    const FIELD_SHOW_USER = 1;
+    const FIELD_SHOW_CLIENT = 2;
+    const FIELD_SHOW_REGISTRATION = 4;
+
+    const FIELD_SHOW_ORDER = 8;
+    const FIELD_SHOW_REGISTERED = 16;
+    const FIELD_SHOW_GUEST = 32;
+    const FIELD_SHOW_SIGNED_IN = 64;
+
+    const FIELD_SHOW_CONTACT_FORM = 128;
+
     protected $name = 'additional-field';
 }
