be0be4fd9739fc4a7c6f1b1f4949b5a71de253eb
Michał Korus <michal.korus@shoper.pl>
2015-08-24 16:16:01 +0200

    add additional_field resource

---
+++ src/DreamCommerce/Resource/AdditionalField.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DreamCommerce\Resource;
+
+use DreamCommerce\Resource;
+
+/**
+ * Resource AdditionalField
+ *
+ * @package DreamCommerce\Resource
+ * @since soon
+ */
+class AdditionalField extends Resource
+{
+    protected $name = 'additional-field';
+}
