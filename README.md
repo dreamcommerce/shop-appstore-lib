8a0d73b986835870c491c5f2045180395d39d7a0
Tomasz Wiktorski <tomasz.wiktorski@dreamcommerce.com>
2018-04-25 08:41:29 +0200

    SHOPER-158649

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ See: http://developers.shoper.pl/
 
 # Changelog
 
+## Version 1.2.5
+* \+ added ``Specialoffers`` resource
+
 ## Version 1.2.4
 * \# fixed parsing response headers
 * \# fixed handling OAuth exception when HTTP response is empty
---
+++ src/DreamCommerce/ShopAppstoreLib/Resource/Specialoffer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DreamCommerce\ShopAppstoreLib\Resource;
+
+use DreamCommerce\ShopAppstoreLib\Resource;
+
+/**
+ * Resource Specialoffer
+ *
+ * @package DreamCommerce\ShopAppstoreLib\Resource
+ * @link https://developers.shoper.pl/developers/api/resources/specialoffers
+ */
+class Specialoffer extends Resource
+{
+    protected $name = 'specialoffers';
+
+}
