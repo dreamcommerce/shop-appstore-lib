9609328922e6eda37442e676649d998a3ace6880
wotekPL <wojciech.martynowski@dreamcommerce.com>
2019-09-02 14:31:58 +0200

    AD-62

---
+++ src/DreamCommerce/ShopAppstoreLib/Resource/Redirect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DreamCommerce\ShopAppstoreLib\Resource;
+
+use DreamCommerce\ShopAppstoreLib\Resource;
+
+/**
+ * Resource Shipping
+ *
+ * @package DreamCommerce\ShopAppstoreLib\Resource
+ * @link https://developers.shoper.pl/developers/api/resources/redirects
+ */
+class Redirect extends Resource
+{
+    protected $name = 'redirects';
+
+}
