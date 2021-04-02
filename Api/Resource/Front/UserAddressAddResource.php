cdf0ea0c66ab8d99882a7f7b0ecae9dc0a26cc2d
kotarb <konrad.kotarba@shoper.pl>
2021-04-02 14:19:08 +0200

    front api improvements

---
+++ Api/Resource/Front/UserAddressAddResource.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the DreamCommerce Shop AppStore package.
+ *
+ * (c) DreamCommerce
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace DreamCommerce\Component\ShopAppstore\Api\Resource\Front;
+
+use DreamCommerce\Component\ShopAppstore\Model\FrontShopInterface;
+use Psr\Http\Message\UriInterface;
+
+class UserAddressAddResource extends FrontResource
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function isPublic(): bool
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMethod(): string
+    {
+        return 'POST';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getUri(FrontShopInterface $shop, array $uriData): UriInterface
+    {
+        $uri = parent::getUri($shop, $uriData);
+        $uri = $uri->withPath($uri->getPath() . '/user/address');
+
+        return $uri;
+    }
+}
---
+++ Api/Resource/Front/UserAddressInfoResource.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the DreamCommerce Shop AppStore package.
+ *
+ * (c) DreamCommerce
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace DreamCommerce\Component\ShopAppstore\Api\Resource\Front;
+
+use DreamCommerce\Component\ShopAppstore\Model\FrontShopInterface;
+use Psr\Http\Message\UriInterface;
+
+class UserAddressInfoResource extends FrontResource
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function isPublic(): bool
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getUri(FrontShopInterface $shop, array $uriData): UriInterface
+    {
+        if(!isset($uriData['id'])) {
+            throw NotDefinedException::forParameter('id');
+        }
+
+        $uri = parent::getUri($shop, $uriData);
+        $uri = $uri->withPath($uri->getPath() . '/user/address/' . $uriData['id']);
+
+        return $uri;
+    }
+}
