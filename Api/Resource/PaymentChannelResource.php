255a60a18331d8be5e1bbe86a68415486518474a
Łukasz Wardowski <lukasz.wardowski@shoper.pl>
2025-02-10 10:15:35 +0100

    SHOPAPI-176

---
+++ Api/Resource/PaymentChannelResource.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the DreamCommerce Shop AppStore package.
+ *
+ * (c) DreamCommerce
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace DreamCommerce\Component\ShopAppstore\Api\Resource;
+
+class PaymentChannelResource extends ItemResource implements ObjectAwareResourceInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'payments/:payment_id/channels';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExternalIdName(): string
+    {
+        return 'channels';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getObjectName(): string
+    {
+        return 'channel';
+    }
+}
