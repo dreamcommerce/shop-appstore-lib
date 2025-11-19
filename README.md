e7cd57ecec6c0ba3545b69c6c888b714ce0a6018
Marcin Materzok <marcin.materzok@shoper.pl>
2025-11-19 20:11:30 +0100

    SHOPAPI-564

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ See: http://developers.shoper.pl/
 
 # Changelog
 
+## Version 1.2.12
+* \+ added ``FileAssets`` resource
+* 
 ## Version 1.2.11
 * \+ added ``Collection`` resource
 
---
+++ src/DreamCommerce/ShopAppstoreLib/Resource/FileAssets.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DreamCommerce\ShopAppstoreLib\Resource;
+
+use DreamCommerce\ShopAppstoreLib\Resource;
+
+/**
+ * Resource FileAssets
+ *
+ * @package DreamCommerce\ShopAppstoreLib\Resource
+ * @link https://developers.shoper.pl/developers/api/resources/file-assets
+ */
+class FileAssets extends Resource
+{
+    protected $name = 'file-assets';
+}
